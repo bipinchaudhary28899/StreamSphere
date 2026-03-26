4d4614ad822c39ba7d2589e0582e5634deba3690
bipin chaudhary <bkumar28899@yahoo.com>
2026-03-26 20:08:14 +0530

    upload bug fixed

--- stream-sphere-client/src/app/services/auth.interceptor.ts
+++ stream-sphere-client/src/app/services/auth.interceptor.ts
@@ -3,6 +3,9 @@ import { HttpRequest, HttpHandlerFn, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export const AuthInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
+  if (request.url.includes('amazonaws.com')) {
+    return next(request);
+  }
   const token = localStorage.getItem('token'); 
   if (token) {
     request = request.clone({
